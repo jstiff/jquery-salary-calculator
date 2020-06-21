49b5fcd0082aba4278e189f92470e90fdb681ab0
“jstiff” <jwstiff8@gmail.com>
2020-06-21 13:11:52 -0500

    altered css

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,40 @@
 $(document).ready(onReady);
 function onReady() {
-  console.log("jquery works!");
-}
-$("#newEmployeeBtn").on("click", addEmployee);
-function addEmployee() {
-  let userFirstName = $("#firstNameInput").val();
-  let userLastName = $("#lastNameInput").val();
-  let userId = $("#idInput").val();
-  let userTitle = $("#titleInput").val();
-  let userSalary = $("#salaryInput").val();
+  let totalCost = 0;
+  let maxCost = 20000;
+  $("#newEmployeeBtn").on("click", addEmployee);
+  function addEmployee() {
+    let userFirstName = $("#firstNameInput").val();
+    let userLastName = $("#lastNameInput").val();
+    let userId = $("#idInput").val();
+    let userTitle = $("#titleInput").val();
+    let userSalary = $("#salaryInput").val();
 
-  let newEmployee = `<tr id="ne">
+    let newEmployee = `<tr id="ne">
   <td>${userFirstName}</td>
   <td>${userLastName}</td>
   <td>${userId}</td>
   <td>${userTitle}</td>
-  <td><span>$</span>${userSalary}</td>
+  <td id="salary"><span>$</span>${userSalary}</td>
   <td><button id="deleteBtn">Delete</button></td>
 </tr>`;
-  $("#tableBody").append(newEmployee);
-  $('section input[type="text"]').val("");
-}
+    $("#tableBody").append(newEmployee);
+    $('section input[type="text"]').val("");
+    totalCost = totalCost + parseInt(userSalary);
+    $("#cost").text(parseInt(totalCost));
+    if (totalCost >= maxCost) {
+      $("#cost").css("background-color", "red");
+    } else if (totalCost < maxCost) {
+      $("#cost").css("background-color", "rgb(224, 231, 238)");
+    }
+  }
 
-$("#tableBody").on("click", "#deleteBtn", deleteEmployee);
-function deleteEmployee(event) {
-  console.log("delete btn works");
-  $(this).closest("tr").remove();
+  $("#tableBody").on("click", "#deleteBtn", deleteEmployee);
+  function deleteEmployee(event) {
+    console.log("delete btn works");
+    let poop = $(this).closest("#salary");
+    $(this).closest("tr").remove();
+
+    console.log(poop);
+  }
 }
